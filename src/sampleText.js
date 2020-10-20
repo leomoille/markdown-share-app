1a9245b3b7604fb89c416d7b9c514462917f3228
Léo Moille <leomoille@outlook.com>
2020-10-20 09:11:43 +0200

    Changing sample text

--- src/sampleText.js
+++ src/sampleText.js
@@ -1,2 +1,2 @@
 export const sampleText =
-  '# Title\n## Subtitle\n\n List:\n  * item\n  * item\n  * item\n\nNumeroted list:\n  1. item\n  2. item\n  3. item\n\nSimple link\n [MarkdownShare](https://leomoille.github.io/markdown-share-app/) \n\nLink : https://leomoille.github.io/markdown-share-app/\n\n```\n console.log("hello world!"); \n``` '
+  '# Title\n## Subtitle\n\n List:\n  * item\n  * item\n  * item\n\nNumerated list:\n  1. item\n  2. item\n  3. item\n\nSimple link\n [MarkdownShare](https://leomoille.github.io/markdown-share-app/) \n\nLink : https://leomoille.github.io/markdown-share-app/\n\n```\n console.log("hello world!"); \n``` '
